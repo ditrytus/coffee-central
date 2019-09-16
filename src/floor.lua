4040f442ed6505825cde7d1938e137e69337a7f1
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2019-09-16 21:00:48 +0200

    Darker floor.

--- src/floor.lua
+++ src/floor.lua
@@ -4,7 +4,7 @@ require("colors")
 local Floor = {
   _draw = function(this)
     fillp(0xcc33) -- big checker
-    rectfill(0, 0, 127, 115, mixColor(peach, white))
+    rectfill(0, 0, 127, 115, mixColor(indigo, light_gray))
     rectfill(0, 115, 127, 127, black)
     fillp(0)
   end
